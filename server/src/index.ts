ef5677d7f7b65e8b0c6ae0e9575bb0b23509dd34
mgnchoi <meganchoi732@gmail.com>
2026-08-15 18:34:08 -0500

    boilerplate/setup

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const PORT = Number(process.env.PORT) || 3000;
+
+function main(): void {
+  const app = express();
+  throw new Error('TODO: not implemented');
+}
+
+main();

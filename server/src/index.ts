58be699fd7b330ffd239044366da429d5e2c375e
mgnchoi <meganchoi732@gmail.com>
2026-08-15 20:25:30 -0500

    set up server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,30 @@
 import express from 'express';
+import path from 'path';
+import { initDb } from './db';
+import { createRoutes } from './routes';
 
+const app = express();
 const PORT = Number(process.env.PORT) || 3000;
+const CLIENT_DIST = path.resolve(__dirname, '../../client/dist');
 
-function main(): void {
-  const app = express();
-  throw new Error('TODO: not implemented');
-}
+// initialize db
+const db = initDb();
 
-main();
+// serve static files
+app.use(express.static(CLIENT_DIST));
+
+// parse request body
+app.use(express.json());
+
+// define route handlers
+app.use('/api', createRoutes(db));
+
+// handler to serve index.html
+app.get('/*splat', (req, res) => {
+  return res.status(200).sendFile(path.join(CLIENT_DIST, 'index.html'));
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running smoothly on http://localhost:${PORT}`);
+});
